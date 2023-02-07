7f1c5447d0724fff8bf14137b298eeeefb5c50cd
kolchenkoav <kolchenkoav@gmail.com>
2023-02-07 23:51:35 +0300

    2.2 Java-based конфигурация Spring MVC done (spring_m2_2)

--- src/main/java/org/example/config/WebContextConfig.java
+++ src/main/java/org/example/config/WebContextConfig.java
@@ -12,14 +12,13 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
-@ComponentScan(basePackages = "org.example.web") //<context:component-scan base-package="org.example.web"/>
-@EnableWebMvc //<mvc:annotation-driven/>
+@ComponentScan(basePackages = "org.example.web")    //<context:component-scan base-package="org.example.web"/>
+@EnableWebMvc                                       //<mvc:annotation-driven/>
 public class WebContextConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/images");
-        //  <mvc:resources mapping="/**" location="classpath:images">
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/images"); //<mvc:resources mapping="/**" location="classpath:images">
     }
 
     @Bean
@@ -36,6 +35,7 @@ public class WebContextConfig implements WebMvcConfigurer {
     public SpringTemplateEngine templateEngine(){
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
+
         return templateEngine;
     }
 
